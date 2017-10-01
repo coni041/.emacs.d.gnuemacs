52d1f27b0c33b3da9f1470b35d76c74c5ba6627b
coni041 <coni041dev@gmail.com>
2017-10-01 20:46:57 +0900

    Add server setting

--- init.el
+++ init.el
@@ -540,12 +540,28 @@
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 
 ;; emacs-server起動
-(require 'server)
-(defun server-ensure-safe-dir (dir) "Noop" t)
-(setq server-socket-dir "~/.emacs.d")
-(unless (server-running-p)
-  (server-start)
-)
+;; server start for emacs-client
+(when window-system                       ; GUI時
+  (require 'server)
+  (defun server-ensure-safe-dir (dir) "Noop" t)
+  (setq server-socket-dir "~/.emacs.d")
+  (unless (eq (server-running-p) 't)
+    (server-start)
+
+    (defun iconify-emacs-when-server-is-done ()
+      (unless server-clients (iconify-frame)))
+
+    ;; C-x C-cに割り当てる(好みに応じて)
+    (global-set-key (kbd "C-x C-c") 'server-edit)
+    ;; M-x exitでEmacsを終了できるようにする
+    (defalias 'exit 'save-buffers-kill-emacs)
+    ;; 起動時に最小化する
+    (add-hook 'after-init-hook 'iconify-emacs-when-server-is-done)
+
+    ;; 終了時にyes/noの問い合わせ
+    (setq confirm-kill-emacs 'yes-or-no-p)
+    )
+  )
 
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 ;;; @ minimap                                                      ;;;
